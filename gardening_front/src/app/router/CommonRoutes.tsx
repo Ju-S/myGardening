25eb98375d0ca3c029b086ce0b629fc1065c1897
Ju-S <wntjd0643@gmail.com>
2025-11-19 17:43:28 +0900

    일정 한글화 중..

--- gardening_front/src/app/router/CommonRoutes.tsx
+++ gardening_front/src/app/router/CommonRoutes.tsx
@@ -6,11 +6,9 @@ const ScheduleRoutes = lazy(() => import("./ScheduleRoutes"));
 
 export function CommonRoutes() {
     return (
-        <main className="mx-auto h-full w-full max-w-3xl px-4 py-12">
-            <Routes>
-                <Route path="/schedule" element={<ScheduleRoutes/>}/>
-                <Route path="*" element={<HomePage/>}/>
-            </Routes>
-        </main>
+        <Routes>
+            <Route path="/schedule" element={<ScheduleRoutes/>}/>
+            <Route path="/*" element={<HomePage/>}/>
+        </Routes>
     );
 }
